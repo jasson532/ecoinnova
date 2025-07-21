f3286ca7286f5526923c1d0955a5f41c2fb70283
Jeison Bolaños <jeisson.bolanos@avaldigitallabs.com>
2025-07-20 21:15:41 -0500

    ✨ add new products and refactor carousel component

--- src/app/constants/products.constants.ts
+++ src/app/constants/products.constants.ts
@@ -2,48 +2,186 @@ import { ICarousel } from "./carousel.constants";
 
 export const listProducts1: ICard[] = [
     {
-        label: 'Tres fuelles cuadrada',
-        path: 'assets/images/products/tres-fuelles-cuadrada/tres-fuelles-cuadrada-3.png',
+        label: 'Plana base fuelle',
+        path: 'assets/images/products/tres-fuelles/1.webp',
         info: [
             {
                 label: 'Tres fuelles cuadrada',
-                path: 'assets/images/products/tres-fuelles-cuadrada/tres-fuelles-cuadrada-1.png'
+                path: 'assets/images/products/tres-fuelles/2.webp'
             },
             {
                 label: 'Tres fuelles cuadrada',
-                path: 'assets/images/products/tres-fuelles-cuadrada/tres-fuelles-cuadrada-2.png'
-            },
-            {
-                label: 'Tres fuelles cuadrada',
-                path: 'assets/images/products/tres-fuelles-cuadrada/tres-fuelles-cuadrada-3.png'
+                path: 'assets/images/products/tres-fuelles/1.webp'
             },
         ]
     },
     {
-        label: 'Plana base fuelle',
-        path: 'assets/images/products/plana-base-fuelle/plana-base-fuelle-2.png',
+        label: 'Tres fuelles',
+        path: 'assets/images/products/tres-fuelles/3.webp',
         info: [
             {
-                label: 'Plana base fuelle',
-                path: 'assets/images/products/plana-base-fuelle/plana-base-fuelle-1.png'
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/1.webp'
             },
             {
-                label: 'Plana base fuelle',
-                path: 'assets/images/products/plana-base-fuelle/plana-base-fuelle-2.png'
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/3.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/4.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/5.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/6.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/7.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/8.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tres-fuelles/9.webp'
             },
         ]
     },
 	{
         label: 'Troquelada',
-        path: 'assets/images/products/troquelada/troquelada-1.png',
+        path: 'assets/images/products/troquelada/1.webp',
         info: [
             {
                 label: 'Troquelada',
-                path: 'assets/images/products/troquelada/troquelada-2.png'
+                path: 'assets/images/products/troquelada/2.webp'
             },
             {
                 label: 'Troquelada',
-                path: 'assets/images/products/troquelada/troquelada-3.png'
+                path: 'assets/images/products/troquelada/3.webp'
+            },
+        ]
+    },
+    {
+        label: 'Fuelle bajo',
+        path: 'assets/images/products/bolsa-fuelle-bajo/1.webp',
+        info: [
+            {
+                label: '-',
+                path: 'assets/images/products/bolsa-fuelle-bajo/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/bolsa-fuelle-bajo/3.webp'
+            },
+        ]
+    },
+    {
+        label: 'Tipo sobre',
+        path: 'assets/images/products/tipo-sobre/1.webp',
+        info: [
+            {
+                label: '-',
+                path: 'assets/images/products/tipo-sobre/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tipo-sobre/1.webp'
+            },
+        ]
+    },
+    {
+        label: 'Plana manija',
+        path: 'assets/images/products/plana-manija/1.webp',
+        info: [
+            {
+                label: '-',
+                path: 'assets/images/products/plana-manija/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/plana-manija/3.webp'
+            },
+        ]
+    },
+    {
+        label: 'Plana troquel 20x25',
+        path: 'assets/images/products/plana-troquel-20-25/1.webp',
+        info: [
+            {
+                label: '-',
+                path: 'assets/images/products/plana-troquel-20-25/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/plana-troquel-20-25/3.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/plana-troquel-20-25/4.webp'
+            },
+        ]
+    },
+    {
+        label: 'Plana troquel 37x50',
+        path: 'assets/images/products/plana-troquel-37-50/1.webp',
+        info: [
+            {
+                label: '-',
+                path: 'assets/images/products/plana-troquel-37-50/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/plana-troquel-37-50/1.webp'
+            },
+        ]
+    },
+    {
+        label: 'Playa-sin-manija',
+        path: 'assets/images/products/playa-sin-manija/1.webp',
+        info: [
+            {
+                label: '-',
+                path: 'assets/images/products/playa-sin-manija/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/playa-sin-manija/1.webp'
+            },
+        ]
+    },
+    {
+        label: 'Tula con ojales',
+        path: 'assets/images/products/tula-con-ojales/1.webp',
+        info: [
+            {
+                label: '-',
+                path: 'assets/images/products/tula-con-ojales/2.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tula-con-ojales/3.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tula-con-ojales/4.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tula-con-ojales/5.webp'
+            },
+            {
+                label: '-',
+                path: 'assets/images/products/tula-con-ojales/6.webp'
             },
         ]
     },
